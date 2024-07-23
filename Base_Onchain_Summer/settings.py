adf7bfe8792e316ed39a801fc9dae877298c2916
nobody <99168300+WhereIsMyMindDL@users.noreply.github.com>
2024-07-23 14:19:23 +0300

    Update settings.py

--- Base_Onchain_Summer/settings.py
+++ Base_Onchain_Summer/settings.py
@@ -8,6 +8,7 @@ module = 1                                                          # 1 - пер
 delay_wallets = [1, 2]                                              # минимальная и максимальная задержка между кошельками
 RETRY_COUNT = 3                                                     # кол-во попыток при возникновении ошибок
 use_only_list_invite_code = False                                   # при True, будет использовать рандомно только рефки из файла, при False будет добавлять рефки новых аккаунтов в файл
+donate_amount = [0.1, 0.3]                                          # мин и макс USDT для доната
 
 #------bot-options------#
 bot_status = False                                                  # True / False
@@ -18,12 +19,17 @@ bot_id     = 0                                                      # telegram i
 registration | первый логин с использованием рефки, запускать один раз
 claim_badge | клейм доступных бейджей
 get_statistics | получить статистику по аккаунту
-speen_the_weel | ежедневные спины, если модуль стоит первым в маршруте, то будет крутить каждый день маршрут , поэтому рекомендую оставлять только спины
+speen_the_weel | ежедневные спины
+donate | донат для получения бейджа | не работает
+
 Quests:
 Mister_Miggles | 0.70$ | 1000 XP
 Mister_Miggles_Song_A_Day | 0.35$ | 250 XP
 Introducing_Coinbase_Wallet_web_app | 0.35$ | 250 XP
 Seasonal_Erosion_Relic_in_Winter | 0$ | 250 XP
+Onchain_Summer_Buildathon | 0.70$ | бейдж
+Team_Liquid_OSPSeries | 0.35$ | 500 XP
+
 Olympic_Games_Paris | 0$ | 250 XP - не работает
 
 Eсли указать модули (не меньше 2ух) в [], то они перемешаются. Пример ниже
@@ -32,12 +38,10 @@ Eсли указать модули (не меньше 2ух) в [], то они
 rotes_modules = [
     ['speen_the_weel'],
     [
-        ['Mister_Miggles'],
-        ['Mister_Miggles_Song_A_Day'],
-        ['Introducing_Coinbase_Wallet_web_app'],
-        ['Seasonal_Erosion_Relic_in_Winter'],
+        ['Onchain_Summer_Buildathon'],
+        ['Team_Liquid_OSPSeries'],
     ],
-    ['get_statistics']
+    ['claim_badge']
 ]
 # =================================== end-options =================================== #
 
