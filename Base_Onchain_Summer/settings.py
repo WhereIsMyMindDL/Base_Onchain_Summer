fb806d6a419e0b51299ee8452b66b164473d4425
nobody <99168300+WhereIsMyMindDL@users.noreply.github.com>
2024-08-10 13:56:16 +0300

    Update settings.py
    One_Year_On_Base

--- Base_Onchain_Summer/settings.py
+++ Base_Onchain_Summer/settings.py
@@ -34,7 +34,6 @@ Team_Liquid_OSPSeries | 0.35$ | 500 XP
 Celebrating_the_Ethereum_ETF | 0.35$ | 250 XP
 ETFEREUM | 0.35$ | 150XP
 Seasonal_Erosion_Relic_in_Spring | 0.00$ | 1000 XP
-
 Seasonal_Erosion_Relic_in_Summer | 0.00$ | 1000 XP
 Ethereum_ETF | 0.35$ | 150XP
 the_world_after_ETH_ETF_approval | 0.35$ | 150XP
@@ -43,6 +42,8 @@ ETH_cant_be_stopped | 0.35$ | 150XP
 Onchain_Summer_Chibling | 0.35$ | 250XP
 Happy_Birthday_Toshi | 0.35$ | 250XP
 EURC_Base_Launch | 0.35$ | 150XP
+
+One_Year_On_Base | 0.29$ | 1000XP
 
 Olympic_Games_Paris | 0$ | 250 XP - не работает
 
